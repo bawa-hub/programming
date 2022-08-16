30515f18011f8b01d3369d98ad7a0e5adc45691f
Vikram Kumar <vikram.kuamr@squareboat.com>
2022-08-16 12:57:00 +0530

    js files deleted

--- _1_language/_6_javascript/_8_nodejs/projects/walleyum-v1.0.3/application/wallet-app/client/components/tabs/TabModule.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TabModule = ({ children }) => (
-  <div>
-    {children}
-  </div>
-);
-
-export default TabModule;
--- _1_language/_6_javascript/_8_nodejs/projects/walleyum-v1.0.3/application/wallet-app/client/utils/inputNumber.js
+++
@@ -1,7 +0,0 @@
-const inputNumber = (e) => {
-  if (!/[0-9.]/.test(e.key)) {
-    e.preventDefault();
-  }
-};
-
-export default inputNumber;
