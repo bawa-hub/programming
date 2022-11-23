7931f6ed6143bb2cb2d9bb98c51be68269deba90
bawa-hub <itsworkingmind@gmail.com>
2022-11-24 00:14:22 +0530

    tree problems added

---
+++ _2_dataStructure/_4_binaryTree/_17_height_or_maximum_depth.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
