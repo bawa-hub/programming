c3cf2e556aa0fc151ca1d1b6292004e3ef9f06e8
bawa-hub <itsworkingmind@gmail.com>
2022-11-26 12:15:15 +0530

    dp problems added

--- _3_algorithms/_0_2_dp/_23_coin_change2.cpp
+++ _3_algorithms/_0_2_dp/_23_coin_change2.cpp
@@ -1,41 +1,123 @@
 // https://leetcode.com/problems/coin-change-2/
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
-const int N = 2510;
+// recursive
+// long countWaysToMakeChangeUtil(vector<int>& arr,int ind, int T, vector<vector<long
+// >>& dp){
 
-int dp[310][10010];
+//     if(ind == 0){
+//         return (T%arr[0]==0);
+//     }
 
-int func(int idx, int amount, vector<int> &coins)
+//     if(dp[ind][T]!=-1)
+//         return dp[ind][T];
+
+//     long notTaken = countWaysToMakeChangeUtil(arr,ind-1,T,dp);
+
+//     long taken = 0;
+//     if(arr[ind] <= T)
+//         taken = countWaysToMakeChangeUtil(arr,ind,T-arr[ind],dp);
+
+//     return dp[ind][T] = notTaken + taken;
+// }
+
+// long countWaysToMakeChange(vector<int>& arr, int n, int T){
+
+//     vector<vector<long>> dp(n,vector<long>(T+1,-1));
+//     return countWaysToMakeChangeUtil(arr,n-1, T, dp);
+// }
+
+// int main() {
+
+//   vector<int> arr ={1,2,3};
+//   int target=4;
+
+//   int n =arr.size();
+
+//   cout<<"The total number of ways is " <<countWaysToMakeChange(arr,n,target);
+// }
+
+// tabulation
+// long countWaysToMakeChange(vector<int>& arr, int n, int T){
+
+//     vector<vector<long>> dp(n,vector<long>(T+1,0));
+
+//     //Initializing base condition
+//     for(int i=0;i<=T;i++){
+//         if(i%arr[0]==0)
+//             dp[0][i]=1;
+//         // Else condition is automatically fulfilled,
+//         // as dp array is initialized to zero
+//     }
+
+//     for(int ind=1; ind<n;ind++){
+//         for(int target=0;target<=T;target++){
+//             long notTaken = dp[ind-1][target];
+
+//             long taken = 0;
+//             if(arr[ind]<=target)
+//                 taken = dp[ind][target-arr[ind]];
+
+//             dp[ind][target] = notTaken + taken;
+//         }
+//     }
+
+//     return dp[n-1][T];
+// }
+
+// int main() {
+
+//   vector<int> arr ={1,2,3};
+//   int target=4;
+
+//   int n =arr.size();
+
+//   cout<<"The total number of ways is " <<countWaysToMakeChange(arr,n,target);
+// }
+
+// space optimized
+long countWaysToMakeChange(vector<int> &arr, int n, int T)
 {
 
-    if (amount == 0)
-        return 1;
+    vector<long> prev(T + 1, 0);
 
-    if (idx < 0)
-        return 0;
-
-    if (dp[idx][amount] != -1)
-        return dp[idx][amount];
-    int ways = 0;
-
-    for (int coin_amount = 0; coin_amount <= amount; coin_amount += coins[idx])
+    // Initializing base condition
+    for (int i = 0; i <= T; i++)
     {
-        ways += func(idx - 1, amount - coin_amount, coins);
+        if (i % arr[0] == 0)
+            prev[i] = 1;
+        // Else condition is automatically fulfilled,
+        // as prev array is initialized to zero
     }
 
-    return dp[idx][amount] = ways;
-}
+    for (int ind = 1; ind < n; ind++)
+    {
+        vector<long> cur(T + 1, 0);
+        for (int target = 0; target <= T; target++)
+        {
+            long notTaken = prev[target];
 
-int coinChange(vector<int> &coins, int amount)
-{
-    memset(dp, -1, sizeof(dp));
-    return func(coins.size() - 1, amount, coins);
+            long taken = 0;
+            if (arr[ind] <= target)
+                taken = cur[target - arr[ind]];
+
+            cur[target] = notTaken + taken;
+        }
+        prev = cur;
+    }
+
+    return prev[T];
 }
 
 int main()
 {
-    vector<int> coins = {2};
-    cout << coinChange(coins, 11);
+
+    vector<int> arr = {1, 2, 3};
+    int target = 4;
+
+    int n = arr.size();
+
+    cout << "The total number of ways is " << countWaysToMakeChange(arr, n, target);
 }
