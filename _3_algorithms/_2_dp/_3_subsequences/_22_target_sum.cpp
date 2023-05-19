692177928ad2080aa9265ea4dd8c81f9e05d9575
Vikram kumar <vikram.kumar@squareboat.com>
2023-05-19 14:31:00 +0530

    dp folder modified

--- _3_algorithms/_2_dp/_3_subsequences/_22_target_sum.cpp
+++ _3_algorithms/_2_dp/_3_subsequences/_22_target_sum.cpp
@@ -1,116 +1,118 @@
 // https://leetcode.com/problems/target-sum/
 
+// exactly same question as count partitions with given difference
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 // recursive
-// int countPartitionsUtil(int ind, int target, vector<int>& arr, vector<vector<int>>
-// &dp){
+int countPartitionsUtil(int ind, int target, vector<int>& arr, vector<vector<int>>
+&dp){
 
-//      if(ind == 0){
-//         if(target==0 && arr[0]==0)
-//             return 2;
-//         if(target==0 || target == arr[0])
-//             return 1;
-//         return 0;
-//     }
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
 
-//     if(dp[ind][target]!=-1)
-//         return dp[ind][target];
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
 
-//     int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
 
-//     int taken = 0;
-//     if(arr[ind]<=target)
-//         taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
 
-//     return dp[ind][target]= (notTaken + taken);
-// }
+    return dp[ind][target]= (notTaken + taken);
+}
 
-// int targetSum(int n,int target, vector<int>& arr){
-//     int totSum = 0;
-//     for(int i=0; i<arr.size();i++){
-//         totSum += arr[i];
-//     }
+int targetSum(int n,int target, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<arr.size();i++){
+        totSum += arr[i];
+    }
 
-//     //Checking for edge cases
-//     if(totSum-target<0) return 0;
-//     if((totSum-target)%2==1) return 0;
+    //Checking for edge cases
+    if(totSum-target<0) return 0;
+    if((totSum-target)%2==1) return 0;
 
-//     int s2 = (totSum-target)/2;
+    int s2 = (totSum-target)/2;
 
-//     vector<vector<int>> dp(n,vector<int>(s2+1,-1));
-//     return countPartitionsUtil(n-1,s2,arr,dp);
-// }
+    vector<vector<int>> dp(n,vector<int>(s2+1,-1));
+    return countPartitionsUtil(n-1,s2,arr,dp);
+}
 
-// int main() {
+int main() {
 
-//   vector<int> arr = {1,2,3,1};
-//   int target=3;
+  vector<int> arr = {1,2,3,1};
+  int target=3;
 
-//   int n = arr.size();
-//   cout<<"The number of ways found is " <<targetSum(n,target,arr);
-// }
+  int n = arr.size();
+  cout<<"The number of ways found is " <<targetSum(n,target,arr);
+}
 
 // tabulation
-// int mod = (int)1e9 + 7;
+int mod = (int)1e9 + 7;
 
-// int findWays(vector<int> &num, int tar)
-// {
-//     int n = num.size();
+int findWays(vector<int> &num, int tar)
+{
+    int n = num.size();
 
-//     vector<vector<int>> dp(n, vector<int>(tar + 1, 0));
+    vector<vector<int>> dp(n, vector<int>(tar + 1, 0));
 
-//     if (num[0] == 0)
-//         dp[0][0] = 2; // 2 cases -pick and not pick
-//     else
-//         dp[0][0] = 1; // 1 case - not pick
+    if (num[0] == 0)
+        dp[0][0] = 2; // 2 cases -pick and not pick
+    else
+        dp[0][0] = 1; // 1 case - not pick
 
-//     if (num[0] != 0 && num[0] <= tar)
-//         dp[0][num[0]] = 1; // 1 case -pick
+    if (num[0] != 0 && num[0] <= tar)
+        dp[0][num[0]] = 1; // 1 case -pick
 
-//     for (int ind = 1; ind < n; ind++)
-//     {
-//         for (int target = 0; target <= tar; target++)
-//         {
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int target = 0; target <= tar; target++)
+        {
 
-//             int notTaken = dp[ind - 1][target];
+            int notTaken = dp[ind - 1][target];
 
-//             int taken = 0;
-//             if (num[ind] <= target)
-//                 taken = dp[ind - 1][target - num[ind]];
+            int taken = 0;
+            if (num[ind] <= target)
+                taken = dp[ind - 1][target - num[ind]];
 
-//             dp[ind][target] = (notTaken + taken) % mod;
-//         }
-//     }
-//     return dp[n - 1][tar];
-// }
+            dp[ind][target] = (notTaken + taken) % mod;
+        }
+    }
+    return dp[n - 1][tar];
+}
 
-// int targetSum(int n, int target, vector<int> &arr)
-// {
-//     int totSum = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         totSum += arr[i];
-//     }
+int targetSum(int n, int target, vector<int> &arr)
+{
+    int totSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totSum += arr[i];
+    }
 
-//     // Checking for edge cases
-//     if (totSum - target < 0 || (totSum - target) % 2)
-//         return 0;
+    // Checking for edge cases
+    if (totSum - target < 0 || (totSum - target) % 2)
+        return 0;
 
-//     return findWays(arr, (totSum - target) / 2);
-// }
+    return findWays(arr, (totSum - target) / 2);
+}
 
-// int main()
-// {
+int main()
+{
 
-//     vector<int> arr = {1, 2, 3, 1};
-//     int target = 3;
+    vector<int> arr = {1, 2, 3, 1};
+    int target = 3;
 
-//     int n = arr.size();
-//     cout << "The number of ways found is " << targetSum(n, target, arr);
-// }
+    int n = arr.size();
+    cout << "The number of ways found is " << targetSum(n, target, arr);
+}
 
 // space optimized
 int mod = (int)1e9 + 7;
