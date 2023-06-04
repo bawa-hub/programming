ed55ad45aadea956419129be539e19261a117c16
Vikram kumar <vikram.kumar@squareboat.com>
2023-06-04 20:34:02 +0530

    tree problems added

--- _2_data_structure/_4_tree/_3_path_problems/_2_binary_tree_paths.cpp
+++ _2_data_structure/_4_tree/_3_path_problems/_2_binary_tree_paths.cpp
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/binary-tree-paths/
+
+class Solution {
+public:
+    void helper(TreeNode* root,vector<string>& answer,string curr){
+        if(!root)return;
+        if(root->left||root->right)curr+=(to_string(root->val)+"->");
+        else{
+            curr+=(to_string(root->val));
+            answer.push_back(curr);
+        }
+        helper(root->left,answer,curr);
+        helper(root->right,answer,curr);  
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> answer;
+        string curr="";
+        helper(root,answer,curr);
+        return answer;
+    }
+};
--- _2_data_structure/_4_tree/_3_path_problems/_5_path_sum3.cpp
+++ _2_data_structure/_4_tree/_3_path_problems/_5_path_sum3.cpp
@@ -1,1 +1,2 @@
 // https://leetcode.com/problems/path-sum-iii/
+// https://leetcode.com/problems/path-sum-iii/solutions/779575/c-3-dfs-based-solutions-explained-and-compared-up-to-100-time-75-space/
