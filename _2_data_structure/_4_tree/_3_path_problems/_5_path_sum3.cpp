b0dd40e85a5d4a3a9189aff47c82c1f82fcc2e1b
Vikram kumar <vikram.kumar@squareboat.com>
2023-06-06 21:26:34 +0530

    tree folders changed

--- _2_data_structure/_4_tree/_3_path_problems/_5_path_sum3.cpp
+++ _2_data_structure/_4_tree/_3_path_problems/_5_path_sum3.cpp
@@ -1,2 +1,34 @@
 // https://leetcode.com/problems/path-sum-iii/
 // https://leetcode.com/problems/path-sum-iii/solutions/779575/c-3-dfs-based-solutions-explained-and-compared-up-to-100-time-75-space/
+
+class Solution {
+public:
+
+    unordered_map<long, int> mp;
+
+    void calculate(TreeNode* root, int targetSum, int& count, long curr){
+
+        if(!root) return;
+        curr+= root->val;
+        
+
+        if(curr == targetSum) count++;
+        if(mp.find(curr-targetSum)!=mp.end()) count+=mp[curr-targetSum];        
+
+        mp[curr]++;
+        
+        calculate(root->left, targetSum, count, curr);
+        calculate(root->right, targetSum, count, curr);
+
+        mp[curr]--;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        
+        int count = 0;
+        
+        calculate(root, targetSum, count, 0);
+
+        return count;
+    }
+};
--- _3_algorithms_and_techniques/_6_graph/_2_topological_sort/_2_topological_sort_bfs_kahns_algo.cpp
+++ _3_algorithms_and_techniques/_6_graph/_2_topological_sort/_2_topological_sort_bfs_kahns_algo.cpp
@@ -24,6 +24,7 @@ public:
                 q.push(i);
             }
         }
+        
         vector<int> topo;
         while (!q.empty())
         {
