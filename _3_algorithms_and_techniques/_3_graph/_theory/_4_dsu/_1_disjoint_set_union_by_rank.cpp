ba361f79a57bfa59163b7913b25ffb4c33e7b4df
Vikram kumar <vikram.kumar@squareboat.com>
2023-06-29 21:53:16 +0530

    tree folder modified

--- _3_algorithms_and_techniques/_3_graph/_theory/_4_dsu/_1_disjoint_set_union_by_rank.cpp
+++ _3_algorithms_and_techniques/_3_graph/_theory/_4_dsu/_1_disjoint_set_union_by_rank.cpp
@@ -26,15 +26,14 @@ public:
     {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
+
         if (ulp_u == ulp_v)
             return;
 
         if (rank[ulp_u] < rank[ulp_v])
             parent[ulp_u] = ulp_v;
-
         else if (rank[ulp_v] < rank[ulp_u])
             parent[ulp_v] = ulp_u;
-
         else
         {
             parent[ulp_v] = ulp_u;
