8792e109f7222ce7877b3e8d2ead82df9e7ce05e
Bawa Vikram <bawa@LAPTOP-7JOC07FH>
2024-01-28 15:17:18 +0530

    folders modified

---
+++ _2_data_structure_new/_1_arrays_and_strings/_6_sliding_window/_3_variable_size_with_simple_calculation/_0_maximum_size_subarray_sum/_0_maximum_size_subarray_sum.cpp
@@ -0,0 +1,32 @@
+// https://www.codingninjas.com/studio/problems/longest-subarray-with-sum-k_6682399
+
+// Note: this will not work for negative numbers
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int i=0,j=0;
+    long long sum = 0;
+    int maxi = 0;
+
+    while(j < a.size()) {
+      sum += a[j];
+
+          if(sum == k) {
+          maxi = max(maxi, j-i+1);
+          }
+      
+
+      if(sum > k) {
+          while (sum > k) {
+                sum -= a[i];
+                 i++;
+            }
+              if(sum == k){
+              maxi = max(maxi, j-i+1);
+            }
+      }
+
+      j++;
+    }
+
+    return maxi;
+}
