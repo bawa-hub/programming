c12516e63ff78b3ea45c1eb094bf63c3834ac825
Vikram <itsworkingmind@gmail.com>
2024-02-05 17:21:48 +0530

    binary search problems added

--- _9_binary_search/_5_bs_on_answers/_8_book_allocation_problem/_8_book_allocation_problem.cpp
+++ _9_binary_search/_5_bs_on_answers/_8_book_allocation_problem/_8_book_allocation_problem.cpp
@@ -1,65 +1,96 @@
 // https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
 // https://www.geeksforgeeks.org/allocate-minimum-number-pages/
+// https://www.codingninjas.com/studio/problems/allocate-books_1090540
 
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPossible(int arr[], int n, int m, int curr_min)
-{
-	int studentsRequired = 1;
-	int curr_sum = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-
-		if (arr[i] > curr_min)
-			return false;
-
-		if (curr_sum + arr[i] > curr_min)
-		{
-			studentsRequired++;
-
-			curr_sum = arr[i];
-
-			if (studentsRequired > m)
-				return false;
-		}
-		else
-			curr_sum += arr[i];
-	}
-	return true;
+// brute force
+int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size(); //size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
 }
 
-int findPages(int arr[], int n, int m)
-{
-	long long sum = 0;
+int findPages(vector<int>& arr, int n, int m) {
+    //book allocation impossible:
+    if (m > n) return -1;
 
-	if (n < m)
-		return -1;
-	int mx = INT_MIN;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
 
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-		mx = max(mx, arr[i]);
-	}
-
-	int start = mx, end = sum;
-	int result = INT_MAX;
-
-	while (start <= end)
-	{
-		int mid = (start + end) / 2;
-		if (isPossible(arr, n, m, mid))
-		{
-			result = mid;
-			end = mid - 1;
-		}
-		else
-			start = mid + 1;
-	}
-
-	return result;
+    for (int pages = low; pages <= high; pages++) {
+        if (countStudents(arr, pages) == m) {
+            return pages;
+        }
+    }
+    return low;
 }
+
+// optimized
+int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size(); //size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    //book allocation impossible:
+    if (m > n) return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> arr = {25, 46, 28, 49, 24};
+    int n = 5;
+    int m = 4;
+    int ans = findPages(arr, n, m);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
+
 
 // Time Complexity: O(N*log (M - max)), where N is the number of different books , max denotes the maximum number of pages from all the books and M denotes the sum of number of pages of all different books
 // Auxiliary Space: O(1)
