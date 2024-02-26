fd3cfe1c1ceea62d22c73dfa10a401723db363a6
Vikram <itsworkingmind@gmail.com>
2024-02-26 20:05:35 +0530

    bfs modified

--- _9_graph/_theory/_1_bfs_dfs/_1_bfs/_bfs.cpp
+++ _9_graph/_theory/_1_bfs_dfs/_1_bfs/_bfs.cpp
@@ -5,21 +5,27 @@ using namespace std;
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> bfsOfGraph(int V, vector<int> adj[], vector<int> &bfs) { 
+
+        // visited array
         int vis[V] = {0}; 
         vis[0] = 1; 
+
+        // queue setup
         queue<int> q;
-        // push the initial starting node 
         q.push(0); 
-        vector<int> bfs; 
+
         // iterate till the queue is empty 
         while(!q.empty()) {
+
            // get the topmost element in the queue 
             int node = q.front(); 
             q.pop(); 
             bfs.push_back(node); 
+
             // traverse for all its neighbours 
             for(auto it : adj[node]) {
+                
                 // if the neighbour has previously not been visited, 
                 // store in Q and mark as visited 
                 if(!vis[it]) {
@@ -53,8 +59,9 @@ int main()
     addEdge(adj, 0, 4);
 
     Solution obj;
-    vector <int> ans = obj.bfsOfGraph(5, adj);
-    printAns(ans);
+    vector<int> bfs;
+    obj.bfsOfGraph(5, adj, bfs);
+    printAns(bfs);
 
     return 0;
 }
