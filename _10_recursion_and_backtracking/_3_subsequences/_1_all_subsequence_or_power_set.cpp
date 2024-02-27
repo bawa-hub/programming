c24e88691368d3886fcaace26a3edfba13c8af57
Vikram <itsworkingmind@gmail.com>
2024-02-27 16:13:03 +0530

    links modified

--- _10_recursion_and_backtracking/_3_subsequences/_1_all_subsequence_or_power_set.cpp
+++ _10_recursion_and_backtracking/_3_subsequences/_1_all_subsequence_or_power_set.cpp
@@ -43,21 +43,22 @@ int main()
 }
 
 // print all possible subsequences of the String
-// void solve(int i, string s, string &f) {
-// 	if (i == s.length()) {
-// 		cout << f << " ";
-// 		return;
-// 	}
-// 	//picking
-// 	f = f + s[i];
-// 	solve(i + 1, s,  f);
-// 	//poping out while backtracking
-// 	f.pop_back();
-// 	solve(i + 1, s,  f);
-// }
-// int main() {
-// 	string s = "abc";
-// 	string f = "";
-// 	cout<<"All possible subsequences are: "<<endl;
-// 	solve(0, s, f);
-// }
+void solve(int i, string s, string &f) {
+	if (i == s.length()) {
+		cout << f << " ";
+		return;
+	}
+	//picking
+	f = f + s[i];
+	solve(i + 1, s,  f);
+	//poping out while backtracking
+	f.pop_back();
+	solve(i + 1, s,  f);
+}
+
+int main() {
+	string s = "abc";
+	string f = "";
+	cout<<"All possible subsequences are: "<<endl;
+	solve(0, s, f);
+}
