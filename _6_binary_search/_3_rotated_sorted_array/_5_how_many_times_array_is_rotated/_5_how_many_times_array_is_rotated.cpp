3514b4446bd4e52af911965ef02d7a825b79dd69
Vikram <itsworkingmind@gmail.com>
2024-02-28 14:04:04 +0530

    binary search problems added

--- _6_binary_search/_3_rotated_sorted_array/_5_how_many_times_array_is_rotated/_5_how_many_times_array_is_rotated.cpp
+++ _6_binary_search/_3_rotated_sorted_array/_5_how_many_times_array_is_rotated/_5_how_many_times_array_is_rotated.cpp
@@ -1,6 +1,25 @@
 // https://practice.geeksforgeeks.org/problems/rotation4723/1?
 // https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
 
+
+// brute force
+int findKRotation(vector<int> &arr) {
+    int n = arr.size(); //size of array.
+    int ans = INT_MAX, index = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < ans) {
+            ans = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We have to iterate through the entire array to check if the target is present in the array.
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
 // same as find minimum in rotated sorted array
 int findKRotation(int arr[], int n)
 {
@@ -41,3 +60,8 @@ int findKRotation(int arr[], int n)
     }
     return idx;
 }
+// Time Complexity: O(logN), N = size of the given array.
+// Reason: We are basically using binary search to find the minimum.
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
--- _6_binary_search/_4_modify_search_space/_1_single_element_in_sorted_array/_1_single_element_in_sorted_array.cpp
+++ _6_binary_search/_4_modify_search_space/_1_single_element_in_sorted_array/_1_single_element_in_sorted_array.cpp
@@ -1,5 +1,50 @@
 // https://leetcode.com/problems/single-element-in-a-sorted-array/
 
+// brute force 1
+int singleNonDuplicate(vector<int>& arr) {
+    int n = arr.size(); //size of the array.
+    if (n == 1) return arr[0];
+
+    for (int i = 0; i < n; i++) {
+
+        //Check for first index:
+        if (i == 0) {
+            if (arr[i] != arr[i + 1])
+                return arr[i];
+        }
+        //Check for last index:
+        else if (i == n - 1) {
+            if (arr[i] != arr[i - 1])
+                return arr[i];
+        }
+        else {
+            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
+                return arr[i];
+        }
+    }
+
+    // dummy return statement:
+    return -1;
+}
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We are traversing the entire array.
+// Space Complexity: O(1) as we are not using any extra space.
+
+// using XOR
+int singleNonDuplicate(vector<int>& arr) {
+    int n = arr.size(); //size of the array.
+    int ans = 0;
+    // XOR all the elements:
+    for (int i = 0; i < n; i++) {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We are traversing the entire array.
+// Space Complexity: O(1) as we are not using any extra space.
+
+// binary search
 class Solution
 {
 public:
@@ -37,3 +82,6 @@ public:
         return -1;
     }
 };
+// Time Complexity: O(logN), N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+// Space Complexity: O(1) as we are not using any extra space.
