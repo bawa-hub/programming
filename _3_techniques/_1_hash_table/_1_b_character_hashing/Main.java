2bd35794556553c339aa3def87881524d89ee17d
Vikram kumar <vikramkumar@Vikrams-MacBook-Air.local>
2024-09-03 21:50:12 +0530

    java code added

--- _3_techniques/_1_hash_table/_1_b_character_hashing/Main.java
+++ _3_techniques/_1_hash_table/_1_b_character_hashing/Main.java
@@ -4,20 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Input the string
         String s = scanner.nextLine();
 
-        // Precompute the frequency of each character
         int[] hash = new int[256]; 
         for (int i = 0; i < s.length(); i++) {
             hash[s.charAt(i)]++;
         }
 
-        // Input the number of queries
         int q = scanner.nextInt();
-        scanner.nextLine();  // Consume the newline character
+        scanner.nextLine();  
 
-        // Process each query
         while (q-- > 0) {
             char c = scanner.nextLine().charAt(0);
             System.out.println(hash[c]);
