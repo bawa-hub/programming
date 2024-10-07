91861eaa63245e30db21a4c7fd0770a73113b3e8
Vikram kumar <vikramkumar@Vikrams-MacBook-Air.local>
2024-10-07 20:54:11 +0530

    tree problem added

--- _8_tree/_2_basic_problems/_1_depth_problem/_3_max_depth_of_nary_tree.cpp
+++ _8_tree/_2_basic_problems/_1_depth_problem/_3_max_depth_of_nary_tree.cpp
@@ -1,1 +1,14 @@
 // https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == nullptr) return 0;
+        int maxi = 0;
+        for(int i=0;i<root->children.size();i++) {
+            maxi = max(maxi, maxDepth(root->children[i]));
+        }
+
+        return 1 + maxi;
+    }
+};
