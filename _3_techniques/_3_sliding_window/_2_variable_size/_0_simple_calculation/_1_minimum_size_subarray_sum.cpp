fd0d1411a4b53a9e339ff8ae32a0923d44a6d9e9
Vikram kumar <vikramkumar@Vikrams-MacBook-Air.local>
2024-10-08 21:47:46 +0530

    sliding windodw problem added

--- _3_techniques/_3_sliding_window/_2_variable_size/_0_simple_calculation/_1_minimum_size_subarray_sum.cpp
+++ _3_techniques/_3_sliding_window/_2_variable_size/_0_simple_calculation/_1_minimum_size_subarray_sum.cpp
@@ -1,27 +1,21 @@
 // https://leetcode.com/problems/minimum-size-subarray-sum/
 
+#include <vector>
+using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int i=0,j=0,n=nums.size(),sum=0,mini=INT_MAX;
+        int i=0,j=0,sum=0,mini=INT_MAX;
 
-        while(j<n) {
-           sum+=nums[j];
-
-           if(sum>=target) {
-               mini = min(mini, j-i+1);
-               while(sum>=target) {
-                   sum-=nums[i];
-                   i++;
-                   if(sum>=target) mini = min(mini, j-i+1);
-               }
-           }
-
-           j++;
+        while(j<nums.size()) {
+             sum += nums[j];
+             while(sum>=target) {
+                mini = min(mini, j-i+1);
+                sum -= nums[i++];
+             }
+             j++;
         }
 
-        if(mini == INT_MAX) return 0;
-
-        return mini;
+        return mini == INT_MAX ? 0 : mini;
     }
 };
