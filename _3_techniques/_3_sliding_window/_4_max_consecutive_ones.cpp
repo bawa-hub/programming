dbd58c5da1fcb4b2d9e77d8bc12863180f45e642
Vikram kumar <vikramkumar@Vikrams-MacBook-Air.local>
2025-03-25 23:02:53 +0530

    mnotes added

--- _3_techniques/_3_sliding_window/_4_max_consecutive_ones.cpp
+++ _3_techniques/_3_sliding_window/_4_max_consecutive_ones.cpp
@@ -1,37 +1,23 @@
 // https://leetcode.com/problems/max-consecutive-ones-iii/
 
-// by me
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int i=0,j=0,n=nums.size(),maxi=INT_MIN,cnt=0, zeroToOne=0;
+        int i=0,j=0,cnt=0,maxi=0,flip=0;
 
-        while(j<n) {
-            if(nums[j]==1) cnt++;
-            else {
-                if(zeroToOne<k) {
-                  cnt++;
-                  zeroToOne++;
-                } else zeroToOne++;
-            }
+        while(j<nums.size()) {
+             if(nums[j]==1) cnt++;
+             else if (nums[j]==0 && flip++ <= k) cnt++;
 
-            // if(zeroToOne == k) {
-                maxi = max(maxi, cnt);
+             while(flip > k) {
+                 if(nums[i]==0) flip--;
+                 i++;
+                 cnt--;
+             }
 
-
-            if(zeroToOne>k) {
-              while(zeroToOne>k) {
-                  if(nums[i]==1) cnt--;
-                  else {
-                      zeroToOne--;
-                  }
-                  i++;
-              }
-            }
-            j++;
+             maxi = max(maxi, j-i+1);
+             j++;
         }
-
-        if(maxi==INT_MIN) return 0;
 
         return maxi;
     }
